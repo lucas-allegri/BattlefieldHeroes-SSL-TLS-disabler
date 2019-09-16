9094d3c83775df97bae7402ad57b1b69398240cd
Lucas Allegri <lucasallegri@protonmail.com>
2019-09-16 18:59:13 -0300

    Merging Jefalo's patch

--- Injector/Origine.cpp
+++ Injector/Origine.cpp
@@ -7,7 +7,6 @@ int main(int argc, TCHAR *argv[])
 	bool done = false;
 	char szProc[MAX_PATH], szDll[MAX_PATH];
 	char* szDllPath1 = (char*)malloc(MAX_PATH);
-	char* szExePath1 = (char*)malloc(MAX_PATH);
 	char* szDll1 = "Hook.dll";
 	szDllPath1 = GetCurrentDir();
 	strcat_s(szDllPath1, MAX_PATH, "\\");
@@ -16,17 +15,21 @@ int main(int argc, TCHAR *argv[])
 	STARTUPINFO info = { sizeof(info) };
 	PROCESS_INFORMATION processInfo;
 
-	if (CreateProcess(argv[1], argv[2], NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo)) {
+
+	wchar_t param1[50];
+	mbstowcs(param1, (char*)argv[1], 49);
+	LPWSTR param1lpwstr = param1;
+
+	wchar_t param2[50];
+	mbstowcs(param2, (char*)argv[2], 49);
+	LPWSTR param2lpwstr = param2;
+
+	if (CreateProcess(param1lpwstr, param2lpwstr, NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo)) {
 		CloseHandle(processInfo.hProcess);
 		CloseHandle(processInfo.hThread);
 	}
 
-	if (InjectDll(processInfo.dwProcessId, szDllPath1))
-	{
-		done = true;
-	}
-
-	if (!done) { std::cout << " > Injection failed!" << std::endl; }
-	system("pause");
+	if (InjectDll(processInfo.dwProcessId, szDllPath1)) { done = true; }
+	if (!done) { std::cout << "Something really weird just happened, oops!" << std::endl; }
 	return 0;
 }
